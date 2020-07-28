df9c693c91cc4c45b0ba4bd3599272eb194aa896
Stefan Mueller <stefan@stefans-entwicklerecke.de>
2020-07-28 12:00:40 +0200

    Remove old load scripts

--- loadPlugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-OUTPUT=$(/usr/bin/nodejs --max-old-space-size=3000 $DIR/getPlugins.js)
-
-if [ "${#OUTPUT}" -gt 1 ]; then
-    echo "$OUTPUT" | ansi2html | mail -s "Etherpad Plugin Loader" -a "Content-Type: text/html; charset=UTF-8" stefan@stefans-entwicklerecke.de
-fi
